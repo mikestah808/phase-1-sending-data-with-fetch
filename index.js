64bb46cca940c8ec6c8ce79ed054ec446a4aeb1a
Michael Stafford <michaelstafford808@gmail.com>
2022-01-25 14:54:28 -1000

    commit tree 997aaf6afd54acfb3cdc6db1e9cc2376303e0e8d

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+function submitData(firstName, email) {
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            "name": firstName,
+            "email": email
+        })
+    };
+    return fetch("http://localhost:3000/users", configObj)
+    .then(res => res.json())
+    .then(body => addNewContact(body.id))
+
+    .catch(response => 
+        console.log(response.message),
+        document.body.append(response.message))
+}
+
+function addNewContact(newID){
+    document.getElementsByTagName("body")[0].innerHTML = newID;
+    document.body.append(message)
+}
+submitData("name", "name@name.com")
+
+
+
+
